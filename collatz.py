bb6b5937e1941f652c2b30b3ee369045a0926f68
Sadek-1801 <hmashrafujjaman@gmail.com>
2025-09-04 00:32:34 +0600

    Practice collage sequence program

--- collatz.py
+++ collatz.py
@@ -1,10 +1,18 @@
 def collatz(number):
   if(number % 2 == 0):
     result = int(number // 2)
-    print(result)
     return result
   elif(number % 2 == 1):
     result = int(3 * number + 1)
-    print(result)
     return result
 
+
+try:
+  user_input = int(input('Enter an integer: '))
+
+  while user_input != 1:
+    user_input = collatz(user_input)
+    print(user_input, end=' ')
+    
+except Exception as err:
+  print("An exception happened " + str(err))
